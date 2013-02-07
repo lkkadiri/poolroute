e2fe0d48c274a17e41ee637ceb600b0d75d561bf
Rasman <lkkadiri@gmail.com>
2013-02-07 09:29:11 -0700

    Added gitignore and first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Poolroute::Application.config.secret_token = 'dce825af47ef4d9e0d1704bae70a78e6c96d1451986e8af1b3729a1ecc95c8a1147da8e017843e4e4ebf692334462278d282d0065890825075c5e7755d712ae6'
